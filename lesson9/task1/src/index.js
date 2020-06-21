931e98625a62c5493904eb1dd67c1542c0f7645a
nesterov1X3 <nesterovsergei99999@gmail.com>
2020-06-21 18:23:00 +0300

    test

---
+++ lesson9/task1/src/index.js
@@ -0,0 +1,24 @@
+// file path: src/index.js
+
+import printProfile from './renderProfile';
+
+const userData = {
+  name: 'Tom',
+  age: 17,
+};
+
+const profile = {
+  ...userData,
+  company: 'Gromcode',
+};
+
+printProfile(profile);
+
+console.log(profile);
+
+
+const number = 17;
+
+if(num ==17){
+    alert('hi')
+}
